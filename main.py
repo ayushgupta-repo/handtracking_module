bce25ae2dbb4bbf15555a0f38770011ed5114cdc
ayushgupta-repo <19ce037@charusat.edu.in>
2022-02-02 19:26:48 +0530

    Detection of hands and points at landmarks showing in prompt is done.

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 
+# to draw line between the landmarks predefined function is used
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     success, img = cap.read()
 
@@ -18,6 +21,19 @@ while True:
 
     # Below will process frames of the image
     results = hands.process(imgRGB)
+
+    # Extracting hands if multiple present
+
+    # Simple result will only return class but to do detection of hand we have to add results.multi_hand_landmarks
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+
+        # handLms = hand numbers
+        for handLms in results.multi_hand_landmarks:
+
+            # using mediapipe we can create those landmark points and joining those points using line
+            mpDraw.draw_landmarks(img, handLms)
 
     cv2.imshow('Image', img)
     cv2.waitKey(1)
