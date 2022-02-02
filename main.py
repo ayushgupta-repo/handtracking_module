5881af397439b4a8add98616c10e06e92ec9fd0c
ayushgupta-repo <19ce037@charusat.edu.in>
2022-02-02 19:10:18 +0530

    Upto processing of frames of RGB image is done.

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+# creating object to create finger locations
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+
 while True:
     success, img = cap.read()
 
-    cv2. imshow('Image', img)
+    # Converting image from BGR to RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Below will process frames of the image
+    results = hands.process(imgRGB)
+
+    cv2.imshow('Image', img)
     cv2.waitKey(1)
